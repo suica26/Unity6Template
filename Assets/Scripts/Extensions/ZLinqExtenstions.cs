515b939c43d0113da0c2eea613b2e52d73c194c7
suica26 <yamamoto.hikaru@icloud.com>
2025-05-14 23:07:08 +0900

    ZLinqにForEach拡張メソッド定義

--- Assets/Scripts/Extensions/ZLinqExtenstions.cs
+++ Assets/Scripts/Extensions/ZLinqExtenstions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using ZLinq;
 
@@ -7,7 +8,7 @@ using ZLinq;
 public static class ZLinqExtensions
 {
     /// <summary>
-    /// ValueEnumerableをIEnumerableに変換する拡張メソッド
+    /// ValueEnumerableをIEnumerableに変換する
     /// </summary>
     public static IEnumerable<T> AsEnumerable<TEnumerator, T>(this ValueEnumerable<TEnumerator, T> valueEnumerable)
         where TEnumerator : struct, IValueEnumerator<T>
@@ -17,6 +18,33 @@ public static class ZLinqExtensions
         while (e.TryGetNext(out var current))
         {
             yield return current;
+        }
+    }
+
+    /// <summary>
+    /// ValueEnumerableの各要素に対して指定されたActionを実行する
+    /// </summary>
+    public static void ForEach<TEnumerator, TSource>(this ValueEnumerable<TEnumerator, TSource> source, Action<TSource> action)
+        where TEnumerator : struct, IValueEnumerator<TSource>
+#if NET9_0_OR_GREATER
+        , allows ref struct
+#endif
+    {
+        using var e = source.Enumerator;
+
+        if (e.TryGetSpan(out var span))
+        {
+            foreach (var item in span)
+            {
+                action(item);
+            }
+        }
+        else
+        {
+            while (e.TryGetNext(out var item))
+            {
+                action(item);
+            }
         }
     }
 }
