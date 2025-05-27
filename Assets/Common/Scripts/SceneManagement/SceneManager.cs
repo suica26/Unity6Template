7ca4c976eb4b7dee8e79e39d535e22d1c2b5dfe0
suica26 <yamamoto.hikaru@icloud.com>
2025-05-27 23:27:40 +0900

    add: 戻る機能の実装

--- Assets/Common/Scripts/SceneManagement/SceneManager.cs
+++ Assets/Common/Scripts/SceneManagement/SceneManager.cs
@@ -13,16 +13,29 @@ public static class SceneManager
 {
     private record CurrentSceneInfo(ISceneContext Context, IScene<ISceneContext> Scene);
 
+    private static ISceneContext? DEFAULT_CONTEXT;
     private static Stack<ISceneContext> LOADED_SCENE_CONTEXTS = new();
     private static CurrentSceneInfo? CURRENT_SCENE_INFO;
     private static bool IS_LOADING = false;
 
     /// <summary>
+    /// デフォルトのシーンコンテキストを設定する
+    /// 1度のみ設定可能
+    /// </summary>
+    public static void SetDefaultContext(ISceneContext context)
+    {
+        if (DEFAULT_CONTEXT != null)
+        {
+            throw new InvalidOperationException("デフォルトコンテキストは一度だけ設定できます。");
+        }
+
+        DEFAULT_CONTEXT = context;
+    }
+
+    /// <summary>
     /// シーンを読み込む
     /// </summary>
-    public static async UniTask LoadSceneAsync<TScene, TContext>(TContext context, CancellationToken ct)
-        where TScene : SceneBase<TContext>
-        where TContext : ISceneContext
+    public static async UniTask LoadSceneAsync(ISceneContext context, CancellationToken ct)
     {
         await UniTask.WaitUntil(() => !IS_LOADING, cancellationToken: ct);
         ct.ThrowIfCancellationRequested();
@@ -36,8 +49,20 @@ public static class SceneManager
             ct.ThrowIfCancellationRequested();
 
             // 新しいシーンをロード
-            await LoadCoreAsync<TScene, TContext>(context, ct);
+            var scene = await LoadCoreAsync(context, ct);
             ct.ThrowIfCancellationRequested();
+
+            // 初期化
+            await scene.InitializeAsync(context, ct);
+            ct.ThrowIfCancellationRequested();
+
+            // 初期化後の処理
+            await scene.PostInitializeAsync(ct);
+            ct.ThrowIfCancellationRequested();
+
+            // 現在のシーン情報を更新
+            CURRENT_SCENE_INFO = new CurrentSceneInfo(context, scene);
+            LOADED_SCENE_CONTEXTS.Push(context);
         }
         catch (Exception e) when (e is not OperationCanceledException)
         {
@@ -48,6 +73,62 @@ public static class SceneManager
             IS_LOADING = false;
         }
     }
+
+    /// <summary>
+    /// 1つ前のシーンに戻る
+    /// </summary>
+    public static async UniTask BackAsync(CancellationToken ct)
+    {
+        if (LOADED_SCENE_CONTEXTS.Count <= 1)
+        {
+            if (DEFAULT_CONTEXT == null)
+            {
+                throw new InvalidOperationException("戻るシーンがありません。デフォルトのコンテキストを設定してください。");
+            }
+            else
+            {
+                // デフォルトシーンに戻る
+                ClearStack();
+                await LoadSceneAsync(DEFAULT_CONTEXT, ct);
+                return;
+            }
+        }
+
+        await UniTask.WaitUntil(() => !IS_LOADING, cancellationToken: ct);
+        ct.ThrowIfCancellationRequested();
+
+        try
+        {
+            IS_LOADING = true;
+
+            // 現在のシーンをアンロード
+            await UnLoadCoreAsync(ct);
+            ct.ThrowIfCancellationRequested();
+
+            // 前のシーンをロード
+            LOADED_SCENE_CONTEXTS.Pop();
+            var previousContext = LOADED_SCENE_CONTEXTS.Peek();
+
+            var scene = await LoadCoreAsync(previousContext, ct);
+            ct.ThrowIfCancellationRequested();
+
+            // 現在のシーン情報を更新
+            CURRENT_SCENE_INFO = new CurrentSceneInfo(previousContext, scene);
+        }
+        catch (Exception e) when (e is not OperationCanceledException)
+        {
+            throw e;
+        }
+        finally
+        {
+            IS_LOADING = false;
+        }
+    }
+
+    /// <summary>
+    /// シーンスタックをクリアする
+    /// </summary>
+    public static void ClearStack() => LOADED_SCENE_CONTEXTS.Clear();
 
     private static async UniTask UnLoadCoreAsync(CancellationToken ct)
     {
@@ -69,9 +150,7 @@ public static class SceneManager
         GC.Collect();
     }
 
-    private static async UniTask LoadCoreAsync<TScene, TContext>(TContext context, CancellationToken ct)
-        where TScene : IScene<TContext>
-        where TContext : ISceneContext
+    private static async UniTask<IScene<ISceneContext>> LoadCoreAsync(ISceneContext context, CancellationToken ct)
     {
         // 新しいシーンをロードする
         await UnitySceneManager
@@ -80,21 +159,12 @@ public static class SceneManager
         ct.ThrowIfCancellationRequested();
 
         // 新しいシーンを取得
-        var scene = (IScene<ISceneContext>)UnityEngine.Object.FindFirstObjectByType<SceneBase<TContext>>();
+        var scene = (IScene<ISceneContext>)UnityEngine.Object.FindFirstObjectByType<SceneBase<ISceneContext>>();
         if (scene == null)
         {
-            throw new InvalidOperationException($"{typeof(TScene)} が見つかりませんでした。");
+            throw new InvalidOperationException($"{context.SceneName} が見つかりませんでした。");
         }
 
-        // ロード時の処理を実行
-        await scene.InitializeAsync(context, ct);
-        ct.ThrowIfCancellationRequested();
-
-        await scene.PostInitializeAsync(ct);
-        ct.ThrowIfCancellationRequested();
-
-        // 現在のシーン情報を更新
-        CURRENT_SCENE_INFO = new CurrentSceneInfo(context, scene);
-        LOADED_SCENE_CONTEXTS.Push(context);
+        return scene;
     }
 }
