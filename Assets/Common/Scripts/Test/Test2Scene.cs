09d5c70a31fa6d58c204bb7a6685cb30422235d1
suica26 <yamamoto.hikaru@icloud.com>
2025-06-02 00:48:28 +0900

    SceneManagerのテスト用シーン

---
+++ Assets/Common/Scripts/Test/Test2Scene.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using Common.Scripts.SceneManagement;
+using Cysharp.Threading.Tasks;
+using System.Threading;
+
+namespace Common.Scripts.Test
+{
+    public class Test2Scene : SceneBase
+    {
+        private static int instanceCount = 0;
+
+        public UniTask InitializeAsync(CancellationToken cancellationToken)
+        {
+            Debug.Log($"{SceneFileName}: InitializeAsync called with context");
+
+            instanceCount++;
+            if (instanceCount > 1)
+            {
+                Debug.Log($"{SceneFileName}: Start Back To Default Scene");
+
+                UniTask.Create(async () =>
+                {
+                    SceneManager.ClearStack();
+
+                    await UniTask.Delay(1000, cancellationToken: cancellationToken);
+                    Debug.Log($"{SceneFileName}: 3000ms delay completed before going back");
+
+                    Debug.Log($"{SceneFileName}: Starting scene backing sequence...");
+                    await SceneManager.BackAsync(cancellationToken);
+                }).Forget();
+
+                return UniTask.CompletedTask;
+            }
+
+            UniTask.Create(async () =>
+            {
+                await UniTask.Delay(1000, cancellationToken: cancellationToken);
+                Debug.Log($"{SceneFileName}: 3000ms delay completed before loading next scene");
+
+                Debug.Log($"{SceneFileName}: Starting scene loading sequence...");
+                await SceneManager.LoadAsync<Test3Scene>((scene, ct) => scene.InitializeAsync(ct));
+            }).Forget();
+
+            return UniTask.CompletedTask;
+        }
+    }
+}
